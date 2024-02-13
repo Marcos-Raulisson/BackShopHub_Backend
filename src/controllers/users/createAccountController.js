3d7f4db93b90664324e64c27cd3862ad288099fb
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2024-02-12 22:59:14 -0300

    Functions to validate email and password before creating an account for the user

--- src/controllers/users/createAccountController.js
+++ src/controllers/users/createAccountController.js
@@ -1,8 +1,19 @@
-const Users = require('../../services/users/createAccountService');
+const CreateAccount = require('../../services/users/createAccountService');
 
 exports.createAccount = (req, res) => {
-  const { email, password } = req.body;
-  // eslint-disable-next-line no-unused-vars
-  const users = new Users(email, password);
-  res.status(200).json({ message: 'Dados recebidos.' });
+  const {
+    name, email, password, confirmPassword,
+  } = req.body;
+
+  if (!name, !email, !password, !confirmPassword) {
+    res.status(400).json({ message: 'All fields are mandatory.' });
+  } else {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const createAccount = new CreateAccount(name, email, password, confirmPassword);
+      res.status(200).json({ message: 'Account created.' });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
 };
--- src/services/users/createAccountService.js
+++ src/services/users/createAccountService.js
@@ -1,17 +1,50 @@
-function Users(email, password) {
+const PasswordValidator = require('password-validator');
+const validator = require('validator');
+
+function CreateAccount(name, email, password, confirmPassword) {
+  this.name = name;
   this.email = email;
   this.password = password;
+  this.confirmPassword = confirmPassword;
 
   this.init();
 }
 
-Users.prototype.init = function () {
-  this.createAccount();
+CreateAccount.prototype.init = function () {
+  this.validateEmail();
+  this.validadePassword();
 };
 
-Users.prototype.createAccount = function () {
-  console.log(`email: ${this.email}
-password: ${this.password}`);
+CreateAccount.prototype.validateEmail = function () {
+  if (!validator.isEmail(this.email)) {
+    throw new Error('Invalid email');
+  }
 };
 
-module.exports = Users;
+CreateAccount.prototype.validadePassword = function () {
+  if (this.password === this.confirmPassword) {
+    const schema = new PasswordValidator();
+
+    schema
+      .is().min(6, 'The password must contain at least 6 characters.')
+      .is().max(100, 'The password must contain a maximum of 100 characters.')
+      .has()
+      .uppercase(1, 'The password must contain uppercase letters.')
+      .has()
+      .lowercase(1, 'The password must contain lowercase letters.')
+      .has()
+      .digits(2, 'Password must contain at least 2 numbers.');
+
+    const validationResult = schema.validate(this.password, { details: true });
+
+    for (let i = 0; i < validationResult.length; i += 1) {
+      if (validationResult[i].message) {
+        throw new Error(validationResult[i].message);
+      }
+    }
+  } else {
+    throw new Error('Passwords must be the same.');
+  }
+};
+
+module.exports = CreateAccount;

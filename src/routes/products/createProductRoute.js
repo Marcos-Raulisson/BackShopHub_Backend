69c9cecb6d833c30348b31a6cf1be0a80a66ebc4
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2024-02-24 11:48:16 -0300

    Implementing JWT token verification middleware.

--- src/routes/products/createProductRoute.js
+++ src/routes/products/createProductRoute.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const createProduct = require('../../controllers/products/create');
 
+const verifyTokenMiddleware = require('../../middlewares/verifyToken');
+
 const router = express.Router();
 
-router.post('/products/create', createProduct.create);
+router.post('/products/create', verifyTokenMiddleware, createProduct.create);
 
 module.exports = router;

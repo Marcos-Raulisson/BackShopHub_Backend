3a23f9540481785557de911281af27826011b8dd
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2024-03-02 14:53:15 -0300

    Configuring Backblaze API integration to store product images in Bucket and send image URL to SGBD server

--- src/controllers/products/createProductController.js
+++ src/controllers/products/createProductController.js
@@ -5,12 +5,12 @@ exports.create = async (req, res) => {
     name, description, price, category, brand, stock,
   } = req.body;
 
-  if (!name || !description || !price || !category || !brand || !stock || !req.file) {
+  if (!name || !req.file || !description || !price || !category || !brand || !stock) {
     return res.status(400).json({ data: { message: 'All field are mandatory.' } });
   }
 
   try {
-    const createProduct = new CreateProduct(name, description, price, category, brand, stock, req.file);
+    const createProduct = new CreateProduct(name, req.file, description, price, category, brand, stock);
     res.status(200).json({ message: 'Created product.' });
   } catch (error) {
     res.status(401).json({ data: { message: error.message } });
--- src/services/products/createProductService.js
+++ src/services/products/createProductService.js
@@ -1,20 +1,63 @@
+require('dotenv').config();
+
+const B2 = require('backblaze-b2');
+
 const RegisterProduct = require('../../models/products/CreateProductModel');
 
-function CreateProduct(name, description, price, category, brand, stock, image) {
-  this.product = [
-    this.name = name,
-    this.description = description,
-    this.price = Number(price).toFixed(2),
-    this.category = category,
-    this.brand = brand,
-    this.stock = parseInt(stock, 10),
-  ];
+function CreateProduct(name, image, description, price, category, brand, stock) {
+  this.name = name;
+  this.image = image;
+  this.description = description;
+  this.price = Number(price).toFixed(2);
+  this.category = category;
+  this.brand = brand;
+  this.stock = parseInt(stock, 10);
 
   this.create();
 }
 
-CreateProduct.prototype.create = function () {
-  const createProduct = new RegisterProduct(this.product);
+CreateProduct.prototype.create = async function () {
+  try {
+    const product = [
+      this.name,
+      await this.uploadImage(),
+      this.description,
+      this.price,
+      this.category,
+      this.brand,
+      this.stock,
+    ];
+
+    const createProduct = new RegisterProduct(product);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+CreateProduct.prototype.uploadImage = async function () {
+  const b2 = new B2({
+    applicationKeyId: process.env.KEY_ID,
+    applicationKey: process.env.APP_KEY,
+  });
+
+  const authResponse = await b2.authorize();
+  const { downloadUrl } = authResponse.data;
+
+  const response = await b2.getUploadUrl({ bucketId: process.env.BUCKET_ID });
+  const { authorizationToken, uploadUrl } = response.data;
+
+  const params = {
+    uploadUrl,
+    uploadAuthToken: authorizationToken,
+    fileName: `products/${this.image.originalname}`,
+    data: this.image.buffer,
+  };
+
+  const fileInfo = await b2.uploadFile(params);
+
+  const url = `${downloadUrl}/file/${process.env.BUCKET_NAME}/${params.fileName}`;
+
+  return url;
 };
 
 module.exports = CreateProduct;

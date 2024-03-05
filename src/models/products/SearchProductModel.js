a162a09e5b3f693e856df3f5e13d423993eb0c75
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2024-03-05 13:41:25 -0300

    Function to check if the product exists in the database

--- src/models/products/SearchProductModel.js
+++ src/models/products/SearchProductModel.js
@@ -4,12 +4,25 @@ function SearchProduct(id) {
   Database.call(this);
 
   this.id = id;
+
+  this.find();
 }
 
 SearchProduct.prototype = Object.create(Database.prototype);
 
-SearchProduct.prototype.find = function () {
-  console.log(this.id);
+SearchProduct.prototype.find = async function () {
+  const connection = await this.openConnection();
+  try {
+    const sql = 'SELECT * FROM products WHERE id = ?';
+    const [rows] = await connection.execute(sql, [this.id]);
+    if (!rows.length > 0) {
+      console.log('Produto não existe');
+    } else {
+      console.log('Produto existe');
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = SearchProduct;
--- src/services/products/UpdateProductService.js
+++ src/services/products/UpdateProductService.js
@@ -3,21 +3,29 @@ const B2 = require('backblaze-b2');
 const SearchProduct = require('../../models/products/SearchProductModel');
 
 function UpdateProduct(id, name, image, description, price, category, brand, stock) {
-  this.id = id;
-  this.name = name;
-  this.image = image;
-  this.description = description;
-  this.price = price;
-  this.category = category;
-  this.brand = brand;
-  this.stock = stock;
+  this.product = [
+    this.id = id,
+    this.name = name,
+    this.image = image,
+    this.description = description,
+    this.price = price,
+    this.category = category,
+    this.brand = brand,
+    this.stock = stock,
+  ];
 
-  this.updateImage();
+  this.updateProduct();
 }
 
-UpdateProduct.prototype.updateImage = function () {
-  const searchProduct = new SearchProduct(this.id);
-  searchProduct.find();
+UpdateProduct.prototype.updateProduct = function () {
+  const searchProduct = new SearchProduct(this.product[0]);
 };
+
+UpdateProduct.prototype.updateImage = function () {
+};
+
+UpdateProduct.prototype.deleteImage = function () {
+};
+
 
 module.exports = UpdateProduct;

b2c9173fa11b1eb209a8371b7fa2ffa0508aaa6e
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2024-04-15 12:27:19 -0300

    Route to return all products

--- src/config/setupServer.js
+++ src/config/setupServer.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const createAccountRoute = require('../routes/users/createAccountRoute');
 const loginRoute = require('../routes/users/authRoute');
+const renewTokenRoute = require('../routes/renewTokenRoute');
 const createProductRoute = require('../routes/products/createProductRoute');
 const updateProductRoute = require('../routes/products/updateProductRoute');
 const deleteProductRoute = require('../routes/products/deleteProductRoute');
-const renewTokenRoute = require('../routes/renewTokenRoute');
+const searchAllProductsRoute = require('../routes/products/searchAllProductsRoute');
 
 const app = express();
 
@@ -14,9 +15,10 @@ app.use(express.json());
 
 app.use(createAccountRoute);
 app.use(loginRoute);
+app.use(renewTokenRoute);
 app.use(createProductRoute);
 app.use(updateProductRoute);
 app.use(deleteProductRoute);
-app.use(renewTokenRoute);
+app.use(searchAllProductsRoute);
 
 module.exports = app;

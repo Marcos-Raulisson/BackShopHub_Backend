b12e668847c4515685078bcfde2fe2ca97e05a60
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2024-04-15 15:22:39 -0300

    Functionality to return all products from the database

--- src/controllers/products/searchAllProductsController.js
+++ src/controllers/products/searchAllProductsController.js
@@ -1,3 +1,20 @@
-exports.searchAllProducts = (req, res) => {
-  res.status(200).json({ data: { message: 'Okay!' } });
+const SearchAllProducts = require('../../models/products/SearchAllProductsModel');
+
+exports.searchAllProducts = async (req, res) => {
+  try {
+    const searchAllProducts = new SearchAllProducts();
+    const allProducts = await searchAllProducts.search();
+
+    if (!allProducts) return res.status(404).json({ data: { message: 'Product not found.' } });
+
+    const productsToSend = allProducts.map((products) => {
+      // eslint-disable-next-line camelcase
+      const { image_id, image_filename, ...rest } = products;
+      return rest;
+    });
+
+    res.status(200).json({ data: { products: productsToSend } });
+  } catch (error) {
+    return res.status(404).json({ data: { message: 'Product not found.' } });
+  }
 };
